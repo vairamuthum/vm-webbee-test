0a5b1f5db876cbc60f247e189de607c4b1e88afe
vairamuthug6 <vairamuthum@geval6.com>
2021-05-16 18:51:47 +0530

    getMenuItems  completed

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 use App\Models\MenuItem;
 use Illuminate\Routing\Controller as BaseController;
 
-class MenuController extends BaseController
-{
+class MenuController extends BaseController {
     /*
     Requirements:
     - the eloquent expressions should result in EXACTLY one SQL query no matter the nesting level or the amount of menu items.
@@ -21,80 +19,81 @@ class MenuController extends BaseController
     - a recursive function in php is needed to structure the query results
     - partial or not working answers also get graded so make sure you commit what you have
 
-
     Sample response on GET /menu:
     ```json
     [
-        {
-            "id": 1,
-            "name": "All events",
-            "url": "/events",
-            "parent_id": null,
-            "created_at": "2021-04-27T15:35:15.000000Z",
-            "updated_at": "2021-04-27T15:35:15.000000Z",
-            "children": [
-                {
-                    "id": 2,
-                    "name": "Laracon",
-                    "url": "/events/laracon",
-                    "parent_id": 1,
-                    "created_at": "2021-04-27T15:35:15.000000Z",
-                    "updated_at": "2021-04-27T15:35:15.000000Z",
-                    "children": [
-                        {
-                            "id": 3,
-                            "name": "Illuminate your knowledge of the laravel code base",
-                            "url": "/events/laracon/workshops/illuminate",
-                            "parent_id": 2,
-                            "created_at": "2021-04-27T15:35:15.000000Z",
-                            "updated_at": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        },
-                        {
-                            "id": 4,
-                            "name": "The new Eloquent - load more with less",
-                            "url": "/events/laracon/workshops/eloquent",
-                            "parent_id": 2,
-                            "created_at": "2021-04-27T15:35:15.000000Z",
-                            "updated_at": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        }
-                    ]
-                },
-                {
-                    "id": 5,
-                    "name": "Reactcon",
-                    "url": "/events/reactcon",
-                    "parent_id": 1,
-                    "created_at": "2021-04-27T15:35:15.000000Z",
-                    "updated_at": "2021-04-27T15:35:15.000000Z",
-                    "children": [
-                        {
-                            "id": 6,
-                            "name": "#NoClass pure functional programming",
-                            "url": "/events/reactcon/workshops/noclass",
-                            "parent_id": 5,
-                            "created_at": "2021-04-27T15:35:15.000000Z",
-                            "updated_at": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        },
-                        {
-                            "id": 7,
-                            "name": "Navigating the function jungle",
-                            "url": "/events/reactcon/workshops/jungle",
-                            "parent_id": 5,
-                            "created_at": "2021-04-27T15:35:15.000000Z",
-                            "updated_at": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        }
-                    ]
-                }
-            ]
-        }
+    {
+    "id": 1,
+    "name": "All events",
+    "url": "/events",
+    "parent_id": null,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": [
+    {
+    "id": 2,
+    "name": "Laracon",
+    "url": "/events/laracon",
+    "parent_id": 1,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": [
+    {
+    "id": 3,
+    "name": "Illuminate your knowledge of the laravel code base",
+    "url": "/events/laracon/workshops/illuminate",
+    "parent_id": 2,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": []
+    },
+    {
+    "id": 4,
+    "name": "The new Eloquent - load more with less",
+    "url": "/events/laracon/workshops/eloquent",
+    "parent_id": 2,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": []
+    }
+    ]
+    },
+    {
+    "id": 5,
+    "name": "Reactcon",
+    "url": "/events/reactcon",
+    "parent_id": 1,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": [
+    {
+    "id": 6,
+    "name": "#NoClass pure functional programming",
+    "url": "/events/reactcon/workshops/noclass",
+    "parent_id": 5,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": []
+    },
+    {
+    "id": 7,
+    "name": "Navigating the function jungle",
+    "url": "/events/reactcon/workshops/jungle",
+    "parent_id": 5,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": []
+    }
+    ]
+    }
+    ]
+    }
     ]
      */
 
     public function getMenuItems() {
+        $menus = MenuItem::whereNull('parent_id')->with('children')->get();
+        echo $menus;die;
         throw new \Exception('implement in coding task 3');
     }
 }
--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -1,12 +1,15 @@
 <?php
 
-
 namespace App\Models;
-
 
 use Illuminate\Database\Eloquent\Model;
 
-class MenuItem extends Model
-{
+class MenuItem extends Model {
+    protected $_table = 'menu_items';
 
+    protected $_primaryKey = 'id';
+
+    public function children() {
+        return $this->hasMany(MenuItem::class, 'parent_id')->with('children');
+    }
 }
